0835e90c0dc170e23909e782e62540b2e2c2d243
Megha Rastogi <rasmegha@3c22fb3758c9.ant.amazon.com>
2021-10-01 15:18:39 -0700

    Adding changes to the dashboard file in the main www folder

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -21,6 +21,7 @@ function onDeviceReady() {
                     <div class="col-12">
                     <a href="playlist.html?courseId=${course.id}"><h4>${course.fullname}</h4></a>
                     <img src="${course.courseimage}" class="img-fluid">
+                    <button type="button" id="download">Download</button>
                     </div>`;
                 });
                 courseWraper += '</div>';
@@ -30,6 +31,34 @@ function onDeviceReady() {
     } else {
         window.location.href = 'index.html';
     }
+
+    $(document).on('click', "#download", (e) => {
+//        var spinnerOptions = { dimBackground: true };
+//                        SpinnerPlugin.activityStart("Downloading...", spinnerOptions);
+                        console.log("in download")
+                        var dl = new download();
+                        dl.Initialize({
+                            fileSystem : cordova.file.externalDataDirectory,
+                            folder: "projects",
+                            unzip: false,
+                            remove: false,
+                            timeout: 0,
+                            success: DownloaderSuccess,
+                            error: DownloaderError,
+                        });
+                        dl.Get("https://lnwebworks.com/sample-project.zip");
+                        function DownloaderError(err) {
+                            console.log("download error: " + err);
+                            alert("download error: " + err);
+                        }
+                        function DownloaderSuccess(evt) {
+                            alert(cordova.file.externalDataDirectory);
+                            console.log("yej");
+                            processZip(cordova.file.externalDataDirectory+ "projects/sample-project.zip", cordova.file.externalDataDirectory+ "projects")
+//                            SpinnerPlugin.activityStop();
+                        }
+    });
+
     $("#logout").on("click", () => {
         localStorage.removeItem("token");
         window.location.href = "index.html";
@@ -58,7 +87,7 @@ function onDeviceReady() {
                         success: DownloaderSuccess,
                         error: DownloaderError,
                     });
-                    dl.Get("https://seminary-tools.000webhostapp.com/zip_files/sample-project.zip");
+                    dl.Get("https://lnwebworks.com/sample-project.zip");
                     function DownloaderError(err) {
                         console.log("download error: " + err);
                         alert("download error: " + err);
@@ -72,39 +101,41 @@ function onDeviceReady() {
             });
         });
         }, (err) => {console.log(err)})
-        function processZip(zipSource, destination) {
-            // Handle the progress event
-            var progressHandler = function(progressEvent){
-                var percent =  Math.round((progressEvent.loaded / progressEvent.total) * 100);
-                // Display progress in the console : 8% ...
-                console.log(percent + "%");
-                if (percent == 100) {
-                    setTimeout(() => {
-                        window.location.href = "offline-project.html";
-                    }, 2000);
-                }
-            };
-            // Proceed to unzip the file
-            window.zip.unzip(zipSource, destination, (status) => {
-                if(status == 0){
-                    console.log("Files succesfully decompressed");
-                    window.resolveLocalFileSystemURL (zipSource, 
-                        function (fileEntry) { 
-                            fileEntry.remove(
-                                function () { 
-                                    console.log('File is removed.');
-                                }, 
-                                function (error) {
-                                    alert('Unable to remove file.');
-                                }
-                            ); 
-                        }, function (error) {console.log(error)}
-                     );
-                }
-                if(status == -1){
-                    console.error("Oops, cannot decompress files");
-                }
-            }, progressHandler);
-        }
-    })
+
+    });
+
+    function processZip(zipSource, destination) {
+                // Handle the progress event
+                var progressHandler = function(progressEvent){
+                    var percent =  Math.round((progressEvent.loaded / progressEvent.total) * 100);
+                    // Display progress in the console : 8% ...
+                    console.log(percent + "%");
+                    if (percent == 100) {
+                        setTimeout(() => {
+                            window.location.href = "offline-project.html";
+                        }, 2000);
+                    }
+                };
+                // Proceed to unzip the file
+                window.zip.unzip(zipSource, destination, (status) => {
+                    if(status == 0){
+                        console.log("Files succesfully decompressed");
+                        window.resolveLocalFileSystemURL (zipSource,
+                            function (fileEntry) {
+                                fileEntry.remove(
+                                    function () {
+                                        console.log('File is removed.');
+                                    },
+                                    function (error) {
+                                        alert('Unable to remove file.');
+                                    }
+                                );
+                            }, function (error) {console.log(error)}
+                         );
+                    }
+                    if(status == -1){
+                        console.error("Oops, cannot decompress files");
+                    }
+                }, progressHandler);
+            }
 }
