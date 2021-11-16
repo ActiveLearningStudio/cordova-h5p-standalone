7dc78488b40417d24ba99927e0f3009f0be6da82
Shikha <shikhawebworksa@gmail.com>
2021-11-16 16:39:05 +0530

    display progress bar on download

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -1,9 +1,7 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    console.log(device.platform);
     var fileSystem = '';
-    var progress;
     switch (device.platform) {
         case "iOS":
             fileSystem = cordova.file.dataDirectory;
@@ -17,13 +15,11 @@ function onDeviceReady() {
         courseContainer = $("#courseContainer"),
         limit = 0,
         offset = 0;
-
     if (token) {
         handleDashboard(token, limit, offset)
     } else {
         window.location.href = 'index.html';
     }
-
 
     function handleDashboard(token, limit, offset) {
         $.ajax({
@@ -56,22 +52,16 @@ function onDeviceReady() {
                         <div class="description">
                             <a href="playlist.html?courseId=${course.id}">
                                 <h5>${course.fullname}</h5>
-                            </a>    
-                            
-                            <button type="button" id="downloadProject" class="btn btn-primary">Download</button>
-                            
+                            </a>                             
+                          <button type="button" id="downloadProject" class="btn btn-primary">Download</button>                         
                         </div>
                     </div>`;
-
-
-
+                    //set image in localstorage
                     localStorage.setItem(course.id, course.courseimage);
-                    console.log(course.fullname)
                     if (counter == 2) {
                         courseWraper += '</div></div>';
                         counter = 0;
                     }
-
                 });
                 courseWraper += '</div>';
                 courseContainer.html(courseWraper);
@@ -80,21 +70,15 @@ function onDeviceReady() {
     }
 
     $(document).on('click', "#downloadProject", (e) => {
-        console.log("in download")
         var modal = document.getElementById("myModal");
-        // Get the button that opens the modal
         // Get the <span> element that closes the modal
         var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks the button, open the modal 
-
         modal.style.display = "block";
         // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
+                modal.style.display = "none";
+            }
+            // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
@@ -118,18 +102,14 @@ function onDeviceReady() {
         }
 
         function DownloaderSuccess(evt) {
-            // alert(fileSystem);
             console.log("yej");
             processZip(fileSystem + "projects/updated-project.zip", fileSystem + "projects")
-                //SpinnerPlugin.activityStop();
         }
     });
-
     $("#logout").on("click", () => {
         localStorage.removeItem("token");
         window.location.href = "index.html";
     });
-
     // function createDirectory(rootDirEntry) {
     //     rootDirEntry.getDirectory('NewDirInRoot', { create: true }, function(dirEntry) {
     //         dirEntry.getDirectory('images', { create: true }, function(subDirEntry) {
@@ -138,21 +118,15 @@ function onDeviceReady() {
     //     }, onErrorGetDir);
     // }
     window.resolveLocalFileSystemURL(fileSystem, (entry) => {
-        entry.getDirectory('Documents', { create: true }, function(dirEntry) {
-            console.log(dirEntry)
-        })
+        entry.getDirectory('Documents', { create: true }, function(dirEntry) {})
     })
     $("#myDownloads").on('click', (e) => {
         e.preventDefault();
-
         window.resolveLocalFileSystemURL(fileSystem + "projects", (entry) => {
             var projectReader = entry.createReader();
             projectReader.readEntries(getProjects = (listProjects) => {
                 console.log(listProjects);
                 if (listProjects.length <= 0) {
-                    //var spinnerOptions = { dimBackground: true };
-                    //SpinnerPlugin.activityStart("Downloading...", spinnerOptions);
-
                     var dl = new download();
                     dl.Initialize({
                         fileSystem: fileSystem,
@@ -167,14 +141,12 @@ function onDeviceReady() {
 
                     function DownloaderError(err) {
                         console.log("download error: " + err);
-                        //alert("download error: " + err);
                     }
 
                     function DownloaderSuccess(evt) {
                         console.log("yej");
 
                         processZip(fileSystem + "projects/sample-project.zip", fileSystem + "projects")
-                            //SpinnerPlugin.activityStop();
 
                     }
                 } else {
@@ -182,9 +154,9 @@ function onDeviceReady() {
                         if (project.isDirectory) {
                             console.log(project);
                             return window.location.href = "offline-project.html";
-                            // return false;
+
                         } else {
-                            console.log(project)
+
                             moveFile(project.nativeURL, activityName[0], fileSystem + "projects");
                         }
                     })
@@ -197,54 +169,22 @@ function onDeviceReady() {
     function processZip(zipSource, destination) {
         // Handle the progress event
         var progressHandler = function(progressEvent) {
-                var percent = Math.round((progressEvent.loaded / progressEvent.total) * 100);
-                // const myProgressBar = document.querySelector(".progress");
-                // progressBar.querySelector(".progress__fill").style.width = `${value}%`;
-                // progressBar.querySelector(".progress__text").textContent = `${value}%`;
-                // const myProgressBar = document.querySelector(".progress");
-                // value = document.getElementById("dene").innerHTML.toString();
-                // updateProgressBar(myProgressBar, value);
-                // console.log("progress bar", myProgressBar)
-                console.log(percent + "%");
-                if (percent == 100) {
-                    setTimeout(() => {
-                        window.location.href = "offline-project.html";
-                    }, 2000);
-                }
+            var percent = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+
+            function updateProgressBar(progressBar, value) {
+                value = Math.round(value);
+                progressBar.querySelector(".progress__fill").style.width = `${value}%`;
+                progressBar.querySelector(".progress__text").textContent = `${value}%`;
             }
-            //updateProgressBar(myProgressBar, percent);
-
-        // function moveFile(fileUri, name, activityPath) {
-        //     window.resolveLocalFileSystemURL(fileUri,
-        //         function(fileEntry) {
-        //             newFileUri = activityPath;
-        //             oldFileUri = fileUri;
-        //             fileExt = "." + "zip";
-
-        //             newFileName = name + fileExt;
-        //             window.resolveLocalFileSystemURL(newFileUri,
-        //                 function(dirEntry) {
-        //                     // move the file to a new directory and rename it
-        //                     fileEntry.moveTo(dirEntry, newFileName, successCallback = (evt) => {
-        //                         processZip(evt.nativeURL, newFileUri + name);
-        //                     }, errorCallback);
-        //                 },
-        //                 errorCallback = (err) => { console.log(err) }
-        //             );
-        //         },
-        //         errorCallback = (err) => { console.log(err) }
-        //     );
-        //     const myProgressBar = document.querySelector(".progress");
-        //     updateProgressBar(myProgressBar, percent);
-        //     console.log(percent + "%");
-        //     if (percent == 100) {
-        //         setTimeout(() => {
-        //             window.location.href = "offline-project.html";
-        //         }, 2000);
-        //     }
-
-        // };
-        // Proceed to unzip the file
+            const myProgressBar = document.querySelector(".progress");
+            updateProgressBar(myProgressBar, percent);
+            console.log(percent + "%");
+            if (percent == 100) {
+                setTimeout(() => {
+                    window.location.href = "offline-project.html";
+                }, 2000);
+            }
+        }
         window.zip.unzip(zipSource, destination, (status) => {
             if (status == 0) {
                 console.log("Files succesfully decompressed");
